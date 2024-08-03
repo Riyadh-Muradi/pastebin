146a036c6239f40d67db30af59ea68fe366185c5
Riyadh Muradi <theahtic@gmail.com>
2024-08-03 16:36:18 +0300

    Button flip change

--- src/components/ButtonFlip.tsx
+++ src/components/ButtonFlip.tsx
@@ -9,18 +9,14 @@ interface ButtonFlipProps {
   className?: string;
 }
 
-const ButtonFlip: React.FC<ButtonFlipProps> = ({
+const ButtonFlip = ({
   title,
-  borderRadius,
+  borderRadius = "8px",
   color,
-  border,
-  textColor,
+  border = true,
+  textColor = "#212121",
   className = "",
-}) => {
-  const [hovered, setHovered] = useState(false);
-
-  const txtColor = textColor ? textColor : "#212121";
-  const brRadius = borderRadius ? borderRadius : "8px";
+}: ButtonFlipProps) => {
   const borderColor = color
     ? `rgba(${parseInt(color.slice(1, 3), 16)}, ${parseInt(color.slice(3, 5), 16)}, ${parseInt(color.slice(5, 7), 16)}, 0.15)`
     : "rgba(33, 33, 33, 0.15)";
@@ -29,18 +25,18 @@ const ButtonFlip: React.FC<ButtonFlipProps> = ({
     <div
       className={`group relative z-0 flex h-9 cursor-pointer flex-col items-center overflow-hidden rounded-[999px] border py-2 ${className}`}
       style={{
-        borderRadius: brRadius,
+        borderRadius,
         border: border === false ? "none" : `1px solid ${borderColor}`,
       }}
     >
       <h3
-        style={{ color: color || txtColor }}
+        style={{ color: color || textColor }}
         className="z-20 translate-y-[0%] rounded-[7px] leading-6 transition-all duration-0 group-hover:-translate-y-[150%] group-hover:duration-300"
       >
         {title}
       </h3>
       <h3
-        style={{ color: color || txtColor }}
+        style={{ color: color || textColor }}
         className="z-20 translate-y-[50%] rounded-[7px] leading-6 transition-all duration-0 group-hover:-translate-y-[100%] group-hover:duration-300"
       >
         {title}
