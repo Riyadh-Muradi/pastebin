4404110714b6b3276e6fc24f13377fcf3e35db72
Riyadh Muradi <theahtic@gmail.com>
2024-08-03 04:48:24 +0300

    404 page and maybe Fetching?

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,10 @@
+"use client";
+import ButtonFlip from "@/components/ButtonFlip";
 import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="relative top-[20vh] flex h-[600px] flex-col items-center justify-center rounded-lg bg-white px-2 py-4 backdrop-blur-lg backdrop-filter sm:px-6 md:px-4 lg:px-6">
+    <div className="relative flex h-[100vh] flex-col items-center justify-center rounded-2xl bg-white/40 px-2 py-4 shadow-sm sm:px-6 md:px-4 lg:px-6">
       <div className="text-center">
         <h1 className="text-8xl font-bold tracking-tighter sm:text-9xl">404</h1>
         <p className="text-black">
@@ -10,10 +12,17 @@ const NotFound = () => {
         </p>
       </div>
       <Link
-        className="mt-4 inline-flex h-10 items-center justify-center rounded-lg bg-black px-8 text-sm font-medium text-gray-50 transition-colors hover:bg-black/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50"
+        className="inline-flex items-center justify-center rounded-2xl py-2"
         href={"/"}
       >
-        Go back home
+        <ButtonFlip
+          className="text-3xl"
+          title="Go Home"
+          borderRadius="8px"
+          color="#000"
+          border={false}
+          textColor=""
+        />
       </Link>
     </div>
   );
