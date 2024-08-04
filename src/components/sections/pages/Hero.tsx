62b412987ceab91d91b0d083d67cd7e2d9d7f3a1
Riyadh Muradi <theahtic@gmail.com>
2024-08-04 12:04:54 +0300

    Update Hero.tsx

--- src/components/sections/pages/Hero.tsx
+++ src/components/sections/pages/Hero.tsx
@@ -52,9 +52,9 @@ const Hero = () => {
             </p>
 
             <Link href={"/new"}>
-              <Button className="rounded-2xl bg-white/80 px-4 py-2 text-xl text-black hover:bg-gray-100">
+              <button className="rounded-2xl bg-white/80 px-4 py-2 text-xl text-black hover:bg-gray-100">
                 Create a new paste
-              </Button>
+              </button>
             </Link>
           </>
         }
