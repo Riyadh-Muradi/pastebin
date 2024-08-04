211d4ba3286cd8b249b924d8038f49ad71944bb3
Riyadh Muradi <theahtic@gmail.com>
2024-08-04 09:20:17 +0300

    Codeblock changes

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import useScrollSync from "@/lib/hooks/useScrollSync";
 import React, { useState, useEffect, useRef } from "react";
 
 interface CodeBlockProps {
@@ -17,6 +18,7 @@ const CodeBlock = ({
   textareaClassName = "",
 }: CodeBlockProps) => {
   const [lineNumbers, setLineNumbers] = useState<number[]>([]);
+  const [language, setLanguage] = useState<string>("TSX");
 
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const lineNumbersRef = useRef<HTMLDivElement | null>(null);
@@ -26,48 +28,55 @@ const CodeBlock = ({
     setLineNumbers(Array.from({ length: lineCount }, (_, i) => i + 1));
   }, [text]);
 
-  useEffect(() => {
-    const syncScroll = () => {
-      if (textareaRef.current && lineNumbersRef.current) {
-        lineNumbersRef.current.scrollTop = textareaRef.current.scrollTop;
-      }
-    };
-
-    const handleScroll = () => {
-      syncScroll();
-    };
-
-    if (textareaRef.current) {
-      textareaRef.current.addEventListener("scroll", handleScroll);
-    }
-
-    return () => {
-      if (textareaRef.current) {
-        textareaRef.current.removeEventListener("scroll", handleScroll);
-      }
-    };
-  }, []);
+  // Use the custom hook to sync the scroll
+  useScrollSync(textareaRef, lineNumbersRef);
 
   return (
-    <div className={`flex overflow-hidden rounded-2xl ${className}`}>
-      {/* Line Numbers */}
-      <div
-        ref={lineNumbersRef}
-        className={`flex flex-col items-end overflow-auto rounded-l-2xl bg-white/40 bg-opacity-10 p-1 px-2 py-4 pl-6 text-sm text-gray-500 shadow-sm ${lineNumberClassName}`}
-      >
-        {lineNumbers.map((number) => (
-          <span key={number} className="mt-[2px] text-sm leading-[0.9]">
-            {number}
-          </span>
-        ))}
+    <div>
+      <div className="mb-2 flex items-center justify-between rounded-2xl bg-white/40 p-4">
+        {/* Language Dropdown */}
+        <select
+          value={language}
+          onChange={(e) => setLanguage(e.target.value)}
+          className="rounded-2xl bg-gray-100 p-2 px-4 text-sm text-gray-500 focus:outline-none"
+        >
+          <option value="TSX">TSX</option>
+          <option value="JavaScript">JavaScript</option>
+          <option value="Python">Python</option>
+          <option value="Java">Java</option>
+          <option value="HTML">HTML</option>
+          <option value="CSS">CSS</option>
+          <option value="JSON">JSON</option>
+        </select>
+
+        {/* Copy and Download Buttons */}
+        <div className="flex gap-4">
+          <button className="text-sm text-gray-500">Copy</button>
+          <button className="text-sm text-gray-500">Download</button>
+        </div>
       </div>
-      {/* Textarea */}
-      <textarea
-        ref={textareaRef}
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        className={`w-full resize-none bg-gray-100/40 bg-opacity-10 p-1 px-2 py-4 text-base leading-[0.9] text-black shadow-sm outline-none ${textareaClassName}`}
-      ></textarea>
+
+      <div className={`flex overflow-hidden rounded-2xl ${className}`}>
+        {/* Line Numbers */}
+        <div
+          ref={lineNumbersRef}
+          className={`flex flex-col items-end overflow-auto rounded-l-2xl bg-white/40 bg-opacity-10 p-1 px-2 py-4 pl-6 text-sm text-gray-500 shadow-sm ${lineNumberClassName}`}
+        >
+          {lineNumbers.map((number) => (
+            <span key={number} className="mt-[2px] text-sm leading-[0.9]">
+              {number}
+            </span>
+          ))}
+        </div>
+
+        {/* Textarea */}
+        <textarea
+          ref={textareaRef}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className={`w-full resize-none bg-gray-100/40 bg-opacity-10 p-1 px-2 py-4 text-base leading-[0.9] text-black shadow-sm outline-none ${textareaClassName}`}
+        ></textarea>
+      </div>
     </div>
   );
 };
