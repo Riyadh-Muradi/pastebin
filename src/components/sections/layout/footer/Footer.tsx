962db712e20b65160f3cb65209e79cacd32cb165
Riyadh Muradi <theahtic@gmail.com>
2024-08-04 11:13:39 +0300

    Navbar and Footer

--- src/components/sections/layout/footer/Footer.tsx
+++ src/components/sections/layout/footer/Footer.tsx
@@ -11,9 +11,9 @@ const Footer = () => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1, delay: 0.5, ease: "easeInOut" }}
-      className="relative z-40 mx-auto flex w-[50%] rounded-2xl bg-white/40 px-[3rem] py-[1rem] text-2xl shadow-sm sm:px-6 md:px-4 lg:px-6"
+      className="mx-auto w-[50%] overflow-hidden rounded-2xl bg-gray-100 bg-opacity-10"
     >
-      <div className="mx-auto w-full max-w-screen-xl xl:pb-2">
+      <div className="relative z-40 flex flex-col rounded-2xl bg-white/40 px-[3rem] py-[1rem] text-2xl shadow-sm sm:px-6 md:px-4 lg:px-6">
         <div className="flex justify-between gap-4 p-4 px-8 py-8">
           <div className="flex flex-col gap-4">
             <Link href="/" className="flex items-center gap-2">
--- src/components/sections/layout/header/Navbar.tsx
+++ src/components/sections/layout/header/Navbar.tsx
@@ -1,55 +1,51 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
 import ButtonFlip from "@/components/ui/buttons/variants/ButtonFlip";
 import { navbarLinks } from "@/data/data";
-import { motion, useAnimation } from "framer-motion";
 import Link from "next/link";
 
 const Navbar = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
   const controls = useAnimation();
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setIsScrolled(true);
+      if (window.scrollY > 0) {
+        setScrolled(true);
       } else {
-        setIsScrolled(false);
+        setScrolled(false);
       }
     };
 
     window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   useEffect(() => {
     controls.start({
-      backgroundColor: isScrolled ? "rgba(255, 255, 255, .4)" : "transparent",
-      padding: isScrolled ? "0.5rem 2rem" : "1rem 3rem",
-      width: isScrolled ? "50%" : "70%",
+      width: scrolled ? "50%" : "70%",
       transition: { duration: 0.5, ease: "easeInOut" },
     });
-  }, [isScrolled, controls]);
+  }, [scrolled, controls]);
 
   return (
     <motion.nav
-      initial={{ opacity: 1 }}
-      transition={{ duration: 1, delay: 0.5, ease: "easeInOut" }}
-      className="sticky top-0 z-50 mx-auto flex items-center justify-between gap-3 rounded-2xl shadow-sm sm:px-6 md:px-4 lg:px-6"
+      className="sticky top-4 z-50 mx-auto overflow-hidden rounded-2xl bg-gray-100 bg-opacity-10"
       animate={controls}
     >
-      <Link href={"./"} className="text-3xl">
-        <ButtonFlip title="Paste" border={false} />
-      </Link>
-      <div className="flex items-center justify-center gap-3 p-2 text-xl">
-        {navbarLinks.map((link) => (
-          <Link key={link.name} href={link.href}>
-            <ButtonFlip title={link.name} border={false} />
-          </Link>
-        ))}
+      <div className="flex items-center justify-between rounded-2xl bg-white/40 px-[3rem] py-[1rem] text-2xl shadow-sm sm:px-6 md:px-4 lg:px-6">
+        <Link href={"./"} className="text-3xl">
+          <ButtonFlip title="Paste" border={false} />
+        </Link>
+        <div className="flex items-center justify-center gap-3 text-xl">
+          {navbarLinks.map((link) => (
+            <Link key={link.name} href={link.href}>
+              <ButtonFlip title={link.name} border={false} />
+            </Link>
+          ))}
+        </div>
       </div>
     </motion.nav>
   );
