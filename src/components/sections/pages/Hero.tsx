5560324a30420367be2f133315096827c0f9c805
Riyadh Muradi <theahtic@gmail.com>
2024-08-04 12:05:48 +0300

    Update Hero.tsx

--- src/components/sections/pages/Hero.tsx
+++ src/components/sections/pages/Hero.tsx
@@ -6,7 +6,6 @@ import { useDrawAnimation } from "@/lib/hooks/useDrawAnimation";
 import { ContainerScroll } from "@/components/ui/container-scroll-animation";
 import CodeBlock from "@/components/CodeBlock";
 import Link from "next/link";
-import { Button } from "@/components/ui/buttons/button";
 
 const Hero = () => {
   const [text, setText] = useState("");
