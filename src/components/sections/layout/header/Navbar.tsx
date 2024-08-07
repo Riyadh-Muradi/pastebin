0ce47f73b093b5486a2a8f4a5008b7ef77a18d59
Riyadh Muradi <theahtic@gmail.com>
2024-08-07 19:03:37 +0300

    Temp nav

--- src/components/sections/layout/header/Navbar.tsx
+++ src/components/sections/layout/header/Navbar.tsx
@@ -46,6 +46,12 @@ const Navbar = () => {
             </Link>
           ))}
         </div>
+        <div className="flex items-center justify-center gap-3 text-xl">
+          Temporary:
+          <Link href="/new">New paste</Link>
+          <Link href="/p/p">Paste</Link>
+          <Link href="/raw/p">Raw</Link>
+        </div>
       </div>
     </motion.nav>
   );
