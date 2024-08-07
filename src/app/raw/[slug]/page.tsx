8e31b03dc2da65a01e499bcfe4f2d8cb65a695c9
Riyadh Muradi <theahtic@gmail.com>
2024-08-07 18:59:39 +0300

    Update page.tsx

---
+++ src/app/raw/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="mx-auto w-[80%] overflow-hidden rounded-2xl bg-gray-100 bg-opacity-10 p-2">
+      <div className="relative flex flex-col rounded-2xl bg-white/40 px-[3rem] py-[1rem] text-center text-9xl shadow-sm sm:px-6 md:px-4 lg:px-6">
+        Raw
+      </div>
+    </div>
+  );
+};
+
+export default Page;
