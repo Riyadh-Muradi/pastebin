d680bf37439273c429e465bd6700fed12d299709
Riyadh Muradi <theahtic@gmail.com>
2024-08-08 09:29:04 +0300

    Removed navbar animation

--- src/components/sections/layout/header/Navbar.tsx
+++ src/components/sections/layout/header/Navbar.tsx
@@ -8,34 +8,36 @@ import { navbarLinks } from "@/config/navbar";
 import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
-  const [scrolled, setScrolled] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const controls = useAnimation();
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
+  // NAVBAR ANIMATION
+  // const controls = useAnimation();
+  // const [scrolled, setScrolled] = useState(false);
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (window.scrollY > 0) {
+  //       setScrolled(true);
+  //     } else {
+  //       setScrolled(false);
+  //     }
+  //   };
 
-  useEffect(() => {
-    controls.start({
-      width: scrolled ? "75%" : "80%",
-      transition: { duration: 0.5, ease: "easeInOut" },
-    });
-  }, [scrolled, controls]);
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, []);
+
+  // useEffect(() => {
+  //   controls.start({
+  //     width: scrolled ? "75%" : "80%",
+  //     transition: { duration: 0.5, ease: "easeInOut" },
+  //   });
+  // }, [scrolled, controls]);
 
   return (
     <motion.nav
-      className="sticky top-4 z-50 mx-auto overflow-hidden rounded-2xl bg-gray-100 bg-opacity-10 p-2"
-      animate={controls}
+      className="sticky w-[80%] top-4 z-50 mx-auto overflow-hidden rounded-2xl bg-gray-100 bg-opacity-10 p-2"
+      // animate={controls}
     >
       <div className="flex items-center justify-between rounded-2xl bg-white/40 px-4 py-2 text-2xl shadow-sm sm:px-6 md:px-4 lg:px-6">
         <Link href={"/"} className="text-3xl">
